e2f17c590687f5808e97ac45e3841636e593cd43
Ronaldo Junior <junhiowr@hotmail.com>
2020-11-11 08:45:48 -0300

    structure done

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => (
+  <View style={styles.container}>
+    <Text>lepolepo</Text>
+    <StatusBar style="auto" />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
